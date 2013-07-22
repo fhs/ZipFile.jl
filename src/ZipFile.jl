32f919254d39aa833b045eee0f2e9e5a5f260dba
Fazlul Shahriar <fshahriar@gmail.com>
2013-07-22 02:48:21 -0400

    implement writing zip files

--- src/ZipFile.jl
+++ src/ZipFile.jl
@@ -1,10 +1,10 @@
 module ZipFile
 
-import Base: readall, close
+import Base: readall, write, close
 import Zlib
 using CRC32
 
-export zipopen, close, readall
+export zipopen, close, readall, write
 
 # TODO: ZIP64 support, data descriptor support
 # TODO: support partial read of File
@@ -12,6 +12,7 @@ export zipopen, close, readall
 const LocalFileHdrSig   = 0x04034b50
 const CentralDirSig     = 0x02014b50
 const EndCentralDirSig  = 0x06054b50
+const ZipVersion = 20
 const Store = 0
 const Deflate = 8
 
@@ -31,10 +32,23 @@ type Dir
 	comment :: String
 end
 
+type WritableFile
+	f :: File
+	closed :: Bool
+end
+
+type WritableDir
+	d :: Dir
+	current :: Union(WritableFile, Nothing)
+	closed :: Bool
+end
+
 readle(ios, ::Type{Uint32}) = htol(read(ios, Uint32))
 readle(ios, ::Type{Uint16}) = htol(read(ios, Uint16))
+writele(ios, x::Uint16) = write(ios, reinterpret(Uint8, [htol(x)]))
+writele(ios, x::Uint32) = write(ios, reinterpret(Uint8, [htol(x)]))
 
-function find_enddiroffset(ios)
+function find_enddiroffset(ios::IOStream)
 	seekend(ios)
 	filesize = position(ios)
 	offset = None
@@ -62,7 +76,7 @@ function find_enddiroffset(ios)
 	offset
 end
 
-function find_diroffset(ios, enddiroffset)
+function find_diroffset(ios::IOStream, enddiroffset::Integer)
 	seek(ios, enddiroffset)
 	if readle(ios, Uint32) != EndCentralDirSig
 		error("internal error")
@@ -72,11 +86,11 @@ function find_diroffset(ios, enddiroffset)
 	skip(ios, 4)
 	offset = readle(ios, Uint32)
 	commentlen = readle(ios, Uint16)
-	comment = string(char(read(ios, Uint8, commentlen))...)
+	comment = utf8(read(ios, Uint8, commentlen))
 	offset, nfiles, comment
 end
 
-function getfiles(ios, diroffset, nfiles)
+function getfiles(ios::IOStream, diroffset::Integer, nfiles::Integer)
 	seek(ios, diroffset)
 	files = Array(File, nfiles)
 	for i in 1:nfiles
@@ -101,14 +115,18 @@ function getfiles(ios, diroffset, nfiles)
 		commentlen = readle(ios, Uint16)
 		skip(ios, 2+2+4)
 		offset = readle(ios, Uint32)
-		name = string(char(read(ios, Uint8, namelen))...)
+		name = utf8(read(ios, Uint8, namelen))
 		skip(ios, extralen+commentlen)
 		files[i] = File(ios, name, compression, crc32, compsize, uncompsize, offset)
 	end
 	files
 end
 
-function zipopen(filename)
+function zipopen(filename::String, new::Bool=false)
+	if new
+		ios = open(filename, "w")
+		return WritableDir(Dir(ios, File[], ""), nothing, false)
+	end
 	ios = open(filename)
 	endoff = find_enddiroffset(ios)
 	diroff, nfiles, comment = find_diroffset(ios, endoff)
@@ -116,8 +134,71 @@ function zipopen(filename)
 	Dir(ios, files, comment)
 end
 
-function close(dir::Dir)
-	close(dir.ios)
+close(dir::Dir) = close(dir.ios)
+
+function close(wd::WritableDir)
+	if wd.closed
+		error("zip file already closed")
+	end
+	wd.closed = true
+	
+	if !is(wd.current, nothing)
+		close(wd.current)
+		wd.current = nothing
+	end
+
+	cdpos = position(wd.d.ios)
+	cdsize = 0
+	
+	# write central directory record
+	for f in wd.d.files
+		writele(wd.d.ios, uint32(CentralDirSig))
+		writele(wd.d.ios, uint16(ZipVersion))
+		writele(wd.d.ios, uint16(ZipVersion))
+		writele(wd.d.ios, uint16(0))
+		writele(wd.d.ios, uint16(f.compression))
+		writele(wd.d.ios, uint16(0))
+		writele(wd.d.ios, uint16(0))
+		writele(wd.d.ios, uint32(f.crc32))
+		writele(wd.d.ios, uint32(f.compressedsize))
+		writele(wd.d.ios, uint32(f.uncompressedsize))
+		b = convert(Vector{Uint8}, f.name)
+		writele(wd.d.ios, uint16(length(b)))
+		writele(wd.d.ios, uint16(0))
+		writele(wd.d.ios, uint16(0))
+		writele(wd.d.ios, uint16(0))
+		writele(wd.d.ios, uint16(0))
+		writele(wd.d.ios, uint32(0))
+		writele(wd.d.ios, uint32(f.offset))
+		write(wd.d.ios, b)
+		cdsize += 46+length(b)
+	end
+	
+	# write end of central directory
+	writele(wd.d.ios, uint32(EndCentralDirSig))
+	writele(wd.d.ios, uint16(0))
+	writele(wd.d.ios, uint16(0))
+	writele(wd.d.ios, uint16(length(wd.d.files)))
+	writele(wd.d.ios, uint16(length(wd.d.files)))
+	writele(wd.d.ios, uint32(cdsize))
+	writele(wd.d.ios, uint32(cdpos))
+	writele(wd.d.ios, uint16(0))
+	
+	close(wd.d.ios)
+end
+
+function close(wf::WritableFile)
+	if wf.closed
+		error("file entry already closed")
+	end
+	wf.closed = true
+	
+	# fill in local file header fillers
+	seek(wf.f.ios, wf.f.offset+14)	# seek to CRC-32
+	writele(wf.f.ios, uint32(wf.f.crc32))
+	writele(wf.f.ios, uint32(wf.f.compressedsize))
+	writele(wf.f.ios, uint32(wf.f.uncompressedsize))
+	seekend(wf.f.ios)
 end
 
 function readall(f::File)
@@ -142,5 +223,43 @@ function readall(f::File)
 	end
 	data
 end
+
+function newfile(wd::WritableDir, name::String)
+	if !is(wd.current, nothing)
+		close(wd.current)
+		wd.current = nothing
+	end
+	
+	f = File(wd.d.ios, name, Store, 0, 0, 0, position(wd.d.ios))
+	
+	# Write local file header. Missing entries will be filled in later.
+	writele(wd.d.ios, uint32(LocalFileHdrSig))
+	writele(wd.d.ios, uint16(ZipVersion))
+	writele(wd.d.ios, uint16(0))
+	writele(wd.d.ios, uint16(f.compression))
+	writele(wd.d.ios, uint16(0))
+	writele(wd.d.ios, uint16(0))
+	writele(wd.d.ios, uint32(f.crc32))	# filler
+	writele(wd.d.ios, uint32(f.compressedsize))	# filler
+	writele(wd.d.ios, uint32(f.uncompressedsize))	# filler
+	b = convert(Vector{Uint8}, f.name)
+	writele(wd.d.ios, uint16(length(b)))
+	writele(wd.d.ios, uint16(0))
+	write(wd.d.ios, b)
+
+	wd.d.files = [wd.d.files, f]
+	wd.current = WritableFile(f, false)
+	wd.current
+end
+
+function write(wf::WritableFile, data::Vector{Uint8})
+	n = write(wf.f.ios, data)
+	wf.f.crc32 = crc32(data, wf.f.crc32)
+	wf.f.uncompressedsize += n
+	wf.f.compressedsize += n
+	n
+end
+
+write(wf::WritableFile, data::String) = write(wf, convert(Vector{Uint8}, data))
 
 end # module
--- test/zipfile_test.jl
+++ test/zipfile_test.jl
@@ -1,4 +1,3 @@
-
 using Base.Test
 using ZipFile
 
@@ -15,6 +14,8 @@ function fileequals(f, s)
 	readall(f) == convert(Vector{Uint8}, s)
 end
 
+
+# test a zip file created using Info-Zip
 dir = zipopen("ziptest.zip")
 @test length(dir.files) == 4
 
@@ -34,3 +35,34 @@ f = findfile(dir, "ziptest/julia.txt")
 @test fileequals(f, repeat("Julia\n", 10))
 
 close(dir)
+
+tmp = mktempdir()
+
+# write an empty zip file
+dir = zipopen("$tmp/empty.zip", true)
+close(dir)
+dir = zipopen("$tmp/empty.zip")
+@test length(dir.files) == 0
+
+
+# write and then read back a zip file
+zipdata = [
+	("hello.txt", "hello world!\n"),
+	("info.txt", "Julia\nfor\ntechnical computing\n"),
+	("julia.txt", "julia\n"^10),
+]
+
+dir = zipopen("$tmp/hello.zip", true)
+for (name, data) in zipdata
+	f = ZipFile.newfile(dir, name)
+	write(f, data)
+end
+close(dir)
+
+dir = zipopen("$tmp/hello.zip")
+for (name, data) in zipdata
+	@test fileequals(findfile(dir, name), data)
+end
+close(dir)
+
+run(`rm -rf $tmp`)
