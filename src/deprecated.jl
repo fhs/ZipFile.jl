017a6f50b4c46967a359c09c42bcbba9a93a02d6
Fazlul Shahriar <fshahriar@gmail.com>
2013-08-23 18:32:49 -0400

    add deprecated types
    NPZ depends on Dir, which is now Reader

--- src/deprecated.jl
+++ src/deprecated.jl
@@ -3,3 +3,7 @@ function open(filename::String, new::Bool=false)
 	Base.warn_once("ZipFile.open is deprecated, use ZipFile.Reader or ZipFile.Writer.")
 	new ? Writer(filename) : Reader(filename)
 end
+
+const Dir = Reader
+const WritableDir = Writer
+const File = ReadableFile
