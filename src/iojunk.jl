1ef1d4991cd94719f7ab1ab57d5e00e301e2effb
Liozou <Liozou@users.noreply.github.com>
2018-08-11 22:54:04 +0100

    Remove Compat

--- src/iojunk.jl
+++ src/iojunk.jl
@@ -7,7 +7,7 @@ end
 function read(f::ReadableFile, ::Type{UInt8})
     # This function needs to be fast because readbytes, readstring, etc.
     # uses it. Avoid function calls when possible.
-    b = Vector{UInt8}(Compat.undef, 1)
+    b = Vector{UInt8}(undef, 1)
     c = read(f, b)
     c[1]
 end
